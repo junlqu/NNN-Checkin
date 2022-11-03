90f8c28e17c0b32d5c0659206f89408402e7670d
Junlin Qu <danielqu2003@gmail.com>
2022-11-03 01:30:54 -0400

    version 1

--- bot.py
+++ bot.py
@@ -1,22 +1,88 @@
+# import libraries
 import discord
-import firebase
+from discord.ext import tasks
+import os
+from dotenv import load_dotenv
+import time
 
-intents = discord.Intents(messages = True, message_content = True, members = True)
+# import files
+from firebase import *
 
+load_dotenv()
+
+intents = discord.Intents(messages = True, message_content = True, members = True, guilds = True, guild_messages = True)
 client = discord.Client(intents=intents)
+
+date = time.localtime()
 
 @client.event
 async def on_ready():
-    print(
-        f'{client.user} is ready!\n'
-    )
+    check_date.start()
+    print(f'{client.user} is ready!\n')
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    if time.localtime().tm_mon != 11:
+        await message.channel.send(f'Go ahead and nut... I am always watching...')
+        return
 
     if message.content.lower() == 'present':
-        await message.channel.send(f'<@{message.author.id}> checked in!')
+        res = update_current_date(message.author.id)
+        if res == 0:
+            await message.channel.send(f'<@{message.author.id}> checked in!')
+        elif res == 1:
+            await message.channel.send(f'<@{message.author.id}> already checked in today!')
+        elif res == 2:
+            await message.channel.send(f'<@{message.author.id}> already failed NNN!\n\nYou lasted {days_lasted(message.author.id)} days.')
+        else:
+            await message.channel.send(f'Error: Invalid command (System error)')
 
-client.run('NzczNzc0NjI4MDk0MjE0MTU0.GGXRda.6tZ4Srfjvth33j0hKGAjSLb7C7HEpZIKUlsi0g')
+    elif message.content.lower() == 'nnn remaining':
+        users = in_chal()
+        if len(users) == 0:
+            await message.channel.send('**All contestants failed!**')
+        else:
+            await message.channel.send(f'**__Remaining contestants:__**\n\n{print_users(users)}')
+
+    elif message.content.lower() == 'nnn failed':
+        users = failed_chal()
+        if len(users) == 0:
+            await message.channel.send('**No one has failed!**')
+        else:
+            await message.channel.send(f'**__Failures:__**\n\n{print_users(users)}')
+    
+    elif message.content.lower() == "nnn help":
+        await message.channel.send('**__Commands:__**\n\n`present`  to check in daily\n\n`NNN remaining`  to check the remaining contestants\n\n`NNN failed`  to check the contestants that already failed')
+
+@tasks.loop(seconds=12)
+async def check_date():
+    date = time.localtime()
+    await client.wait_until_ready()
+    channel = client.get_channel(1037197294606483536) # Channel from ID
+    if (date.tm_hour == 0) and (date.tm_min == 0) and (date.tm_sec < 12):
+        failed = users_failed()
+        if len(failed) == 0:
+            await channel.send("Congratulations! All remaining contestants passed!")
+        else:
+            await channel.send(f"Uh Oh! Somebody couldn't hold the urges...\n\nThe following contestants failed:\n\n{print_users_mention(failed)}")
+
+def print_users(users):
+    conc = ""
+    for user in users:
+        conc += '`' + client.get_user(int(user)).display_name + '`\n\n'
+    return conc
+
+def print_users_mention(users):
+    conc = ""
+    for user in users:
+        conc += f"<@{str(user)}>\n"
+    return conc
+
+def add_roles(ids, role):
+    for id in ids:
+        client.get_user(int(id)).add_role(role)
+
+client.run(os.getenv('TOKEN'))
---
+++ firebase.py
@@ -0,0 +1,108 @@
+import os
+from dotenv import load_dotenv
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import time
+
+load_dotenv()
+
+config = {
+  "type": os.getenv('TYPE'),
+  "project_id": os.getenv('PROJECT_ID'),
+  "private_key_id": os.getenv('PRIVATE_KEY_ID'),
+  "private_key": os.getenv('PRIVATE_KEY'),
+  "client_email": os.getenv('CLIENT_EMAIL'),
+  "client_id": os.getenv('CLIENT_ID'),
+  "auth_uri": os.getenv('AUTH_URI'),
+  "token_uri": os.getenv('TOKEN_URI'),
+  "auth_provider_x509_cert_url": os.getenv('AUTH_PROVIDER'),
+  "client_x509_cert_url": os.getenv('CLIENT_URL')
+}
+url = os.getenv('URL')
+cred = credentials.Certificate(config)
+
+app = firebase_admin.initialize_app(
+    cred,
+    {'databaseURL' : url}
+)
+
+def create_dictionary():
+    date = time.localtime().tm_mday
+    d = {}
+    for i in range(1, 31):
+        d[str(i)] = True if i <= date else False
+    return d
+
+def set_up(id):
+    ref = db.reference(f'/')
+    ref.push(id)
+    ref = db.reference(f'/{id}')
+    ref.update(create_dictionary())
+
+# Return 0 if newly updated, 1 if already updated, 2 if already failed
+def update_current_date(id):
+    ref = db.reference(f'/{id}')
+    if ref.get() == None:
+        set_up(id)
+        return 0
+    else:
+        date = time.localtime().tm_mday
+        ref1 = ref.child(f'{date}')
+        ref0 = ref.child(f'{date - 1}')
+        
+        if ref0.get() == False:
+            return 2
+        if ref1.get() == True:
+            return 1
+
+        ref.update({date: True})
+        return 0
+
+# Return the number of days the person lasted
+def days_lasted(id):
+    counter = 0
+    r = db.reference(f'/{id}')
+    while (r.child(f'{counter + 1}').get()):
+        counter = counter + 1
+    return counter
+
+# Returns filtered dictionary of only user ids
+def user_ids(dic):
+    new = dict()
+    for (key, val) in dic.items():
+        if key.isnumeric():
+            new[key] = val
+    return new
+
+# Return a list of users that failed to check in
+def users_failed():
+    failed = []
+    ref = db.reference(f'/')
+    users = user_ids(ref.get())
+    print(users)
+    for (user, vals) in users.items():
+        if vals[time.localtime().tm_mday - 1] == False and vals[time.localtime().tm_mday - 2] == True:
+            failed.append(user)
+    print(failed)
+    return failed
+
+# Return a list of users that failed the challenge
+def failed_chal():
+    failed = []
+    ref = db.reference(f'/')
+    users = user_ids(ref.get())
+    for (user, vals) in users.items():
+        if vals[time.localtime().tm_mday - 1] == False:
+            failed.append(user)
+    return failed
+
+# Return a list of the users that are still in the challenge
+def in_chal():
+    cont = []
+    ref = db.reference(f'/')
+    users = user_ids(ref.get())
+    for (user, vals) in users.items():
+        if vals[time.localtime().tm_mday - 1] == True:
+            cont.append(user)
+    return cont
