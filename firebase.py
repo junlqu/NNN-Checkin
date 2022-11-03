33599801e906edef57b38951addba5b8dbf782ff
Junlin Qu <17263194+xzyio@users.noreply.github.com>
2022-11-03 03:53:32 -0400

    updated os module

--- firebase.py
+++ firebase.py
@@ -8,18 +8,18 @@ import time
 load_dotenv()
 
 config = {
-  "type": os.getenv('TYPE'),
-  "project_id": os.getenv('PROJECT_ID'),
-  "private_key_id": os.getenv('PRIVATE_KEY_ID'),
-  "private_key": os.getenv('PRIVATE_KEY'),
-  "client_email": os.getenv('CLIENT_EMAIL'),
-  "client_id": os.getenv('CLIENT_ID'),
-  "auth_uri": os.getenv('AUTH_URI'),
-  "token_uri": os.getenv('TOKEN_URI'),
-  "auth_provider_x509_cert_url": os.getenv('AUTH_PROVIDER'),
-  "client_x509_cert_url": os.getenv('CLIENT_URL')
+  "type": os.getenv("TYPE"),
+  "project_id": os.getenv("PROJECT_ID"),
+  "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+  "private_key": os.getenv("PRIVATE_KEY"),
+  "client_email": os.getenv("CLIENT_EMAIL"),
+  "client_id": os.getenv("CLIENT_ID"),
+  "auth_uri": os.getenv("AUTH_URI"),
+  "token_uri": os.getenv("TOKEN_URI"),
+  "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER"),
+  "client_x509_cert_url": os.getenv("CLIENT_URL")
 }
-url = os.getenv('URL')
+url = os.getenv("URL")
 cred = credentials.Certificate(config)
 
 app = firebase_admin.initialize_app(
